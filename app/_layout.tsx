6ecef33b3521aaff8b5985adb32fdbf22a705e43
yujunggg <a90134627@gmail.com>
2025-12-02 00:39:26 +0900

    feat: add layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,37 @@
+import AppHeader from "@/components/AppHeader";
+import TabBar from "@/components/TabBar";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 import "react-native-reanimated";
 
 export default function RootLayout() {
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="camera"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="drug-risk-analysis"
-          options={{
-            headerShown: true,
-            title: "약물 위험 분석",
-            headerBackTitle: "뒤로",
-          }}
-        />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <AppHeader />
+      <View style={{ flex: 1 }}>
+        <Stack screenOptions={{ animation: "none", headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="camera" />
+          <Stack.Screen
+            name="drug-risk-analysis"
+            options={{
+              headerShown: true,
+              title: "약물 위험 분석",
+              headerBackTitle: "뒤로",
+            }}
+          />
+          <Stack.Screen name="schedule" />
+          <Stack.Screen name="medicine" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
+        </Stack>
+      </View>
+      <TabBar />
       <StatusBar style="auto" />
-    </>
+    </View>
   );
 }
