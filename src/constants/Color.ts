d9e82a61cebe5b6ea2be6018069303d6db9d1b8a
yujunggg <a90134627@gmail.com>
2025-12-01 16:00:00 +0900

    feat: add Color design System

---
+++ src/constants/Color.ts
@@ -0,0 +1,15 @@
+export const Colors = {
+  primary: "#FF4242",
+  second: "#FF6249",
+
+  white1: "#FFFFFF",
+  white2: "#F5F5F5",
+
+  black1: "#000000",
+  black2: "#1F1F1F",
+
+  gray1: "#242424",
+  gray2: "#9B9B9B",
+  gray3: "#CFCFCF",
+  gray4: "#F3F4F6",
+} as const;
